16ac840ae778697b56b18744a1ba7c8e23cbab40
Mohamed Amine Dridi <78367821+modridi@users.noreply.github.com>
2023-04-13 17:32:33 +0200

    feat(azure)!: replace pod identity with workload identity (#42)
    * feat(azure)!: replace pod identity with workload identity
    
    * docs(terraform-docs): generate docs and write to README.adoc
    
    ---------
    
    Co-authored-by: modridi <modridi@users.noreply.github.com>

--- aks/extra_variables.tf
+++ aks/extra_variables.tf
@@ -7,3 +7,8 @@ variable "dns_zone_resource_group_name" {
   description = "The Azure DNS zone's resource group name."
   type        = string
 }
+
+variable "cluster_oidc_issuer_url" {
+  description = "The OIDC issuer URL that is associated with the cluster."
+  type        = string
+}
