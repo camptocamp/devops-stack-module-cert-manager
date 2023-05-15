3b6a40166ae472bc2d3b30bcc681710411a41a57
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2023-05-15 17:44:29 +0200

    fix: point id to the right id and add description to outputs

--- aks/outputs.tf
+++ aks/outputs.tf
@@ -1,3 +1,4 @@
 output "id" {
-  value = module.cert-manager.id
+  description = "ID to pass other modules in order to refer to this module as a dependency."
+  value       = module.cert-manager.id
 }
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
 output "id" {
-  value = argocd_application.this.id
+  description = "ID to pass other modules in order to refer to this module as a dependency."
+  value       = resource.null_resource.this.id
 }
