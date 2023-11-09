2e1f7c8445e9b06e2f7c7ebbb7888b89023ffbfd
Lenny Consuegra <lenny.consuegra@camptocamp.com>
2023-11-09 11:41:27 +0100

    feat: use loop to build issuers values and output them

--- scaleway/outputs.tf
+++ scaleway/outputs.tf
@@ -2,3 +2,8 @@ output "id" {
   description = "ID to pass other modules in order to refer to this module as a dependency."
   value       = module.cert-manager.id
 }
+
+output "cluster_issuers" {
+  description = "List of cluster issuers created by cert-manager."
+  value       = module.cert-manager.cluster_issuers
+}
