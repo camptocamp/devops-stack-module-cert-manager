8ccf6aa146d5679688c9c2f0ea058870c0e3a558
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2024-10-21 14:18:25 +0200

    fix: remove unsupported values from the cert-manager field

--- self-signed/locals.tf
+++ self-signed/locals.tf
@@ -1,12 +1,12 @@
 locals {
   helm_values = [{
-    cert-manager = {
-      tlsCrt = base64encode(tls_self_signed_cert.root.cert_pem)
-      tlsKey = base64encode(tls_private_key.root.private_key_pem)
-      clusterIssuers = {
-        letsencrypt = {
-          enabled = false
-        }
+    cert-manager = {}
+
+    # This structure will be merged with the one with the same name on the root locals.tf.
+    clusterIssuers = {
+      ca = {
+        tlsCrt = base64encode(tls_self_signed_cert.root.cert_pem)
+        tlsKey = base64encode(tls_private_key.root.private_key_pem)
       }
     }
   }]
